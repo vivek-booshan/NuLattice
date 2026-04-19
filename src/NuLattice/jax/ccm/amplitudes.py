d248b095844bd0e9f43dadba09e03ea56e7c441c
vivek-booshan <vivek.booshan@gmail.com>
2026-04-19 00:04:30 -0400

    add conditional sharding constraint (pass through tensor if shard is None)

--- src/NuLattice/jax/ccm/amplitudes.py
+++ src/NuLattice/jax/ccm/amplitudes.py
@@ -27,6 +27,11 @@ def add_AB_IJ(target, val):
     )
 
 
+def cond_sharding_constraint(tensor, shard):
+    if shard is not None:
+        return with_sharding_constraint(tensor, shard)
+    return tensor
+
 @jax.jit
 def t1Iter(t1, t2, f_ph, f_pp, f_hh, v_phph, v_phhh, v_pphh, v_ppph):
     indices, values = v_ppph
@@ -90,7 +95,7 @@ def t2_H2_ppph(H2, t1, t2, v_ppph, shard_pphh):
     # Diagram 3
     d3_v = jnp.zeros((pnum, pnum)).at[idx_d, idx_a].add(values * t1[idx_c, idx_k])
     d3_int = jnp.einsum("da, dbij -> abij", d3_v, t2)
-    d3_int = with_sharding_constraint(d3_int, shard_pphh)
+    d3_int = cond_sharding_constraint(d3_int, shard_pphh)
     H2 = add_AB(H2, -d3_int)
 
     # Diagram 4
@@ -100,7 +105,7 @@ def t2_H2_ppph(H2, t1, t2, v_ppph, shard_pphh):
         .add(values[:, None] * t1[idx_c, :])
     )
     d4_int = jnp.einsum("acik, bcjk -> abij", d4_v, t2)
-    d4_int = with_sharding_constraint(d4_int, shard_pphh)
+    d4_int = cond_sharding_constraint(d4_int, shard_pphh)
     H2 = add_AB_IJ(H2, d4_int)
 
     # Diagram 5
@@ -110,7 +115,7 @@ def t2_H2_ppph(H2, t1, t2, v_ppph, shard_pphh):
         .add(values[:, None, None] * t2[idx_c, idx_d, :, :])
     )
     d5_int = jnp.einsum("bijk, ak -> abij", d5_v, t1)
-    d5_int = with_sharding_constraint(d5_int, shard_pphh)
+    d5_int = cond_sharding_constraint(d5_int, shard_pphh)
     H2 = add_AB(H2, 0.5 * d5_int)
 
     # Diagram 6
@@ -122,7 +127,7 @@ def t2_H2_ppph(H2, t1, t2, v_ppph, shard_pphh):
         .add(values[:, None, None] * (t1_c[:, :, None] * t1_d[:, None, :]))
     )
     d6_int = jnp.einsum("bijk, ak -> abij", d6_v, t1)
-    d6_int = with_sharding_constraint(d6_int, shard_pphh)
+    d6_int = cond_sharding_constraint(d6_int, shard_pphh)
     H2 = add_AB_IJ(H2, 0.5 * d6_int)
 
     return H2
@@ -167,26 +172,26 @@ def t2_update(t2, X_hh, X_pp, H2):
 def t2_H2_dense_part1(H2, t1, t2, v_pphh, v_phph, v_phhh, v_hhhh, shard_pphh):
     """Diagrams 1 to 5"""
     d1 = jnp.einsum("klij, abkl -> abij", v_hhhh, t2)
-    d1 = with_sharding_constraint(d1, shard_pphh)
+    d1 = cond_sharding_constraint(d1, shard_pphh)
     H2 = H2.at[:].add(0.5 * d1)
     
     d2 = jnp.einsum('bkcj, acik -> abij', v_phph, t2)
-    d2 = with_sharding_constraint(d2, shard_pphh)
+    d2 = cond_sharding_constraint(d2, shard_pphh)
     H2 = add_AB_IJ(H2, d2)
     
     d3 = jnp.einsum("bkij, ak -> abij", v_phhh, t1)
-    d3 = with_sharding_constraint(d3, shard_pphh)
+    d3 = cond_sharding_constraint(d3, shard_pphh)
     H2 = add_AB(H2, d3)
     
     d4_int = jnp.einsum("acik, cdkl -> adil", t2, v_pphh)
-    d4_int = with_sharding_constraint(d4_int, shard_pphh)
+    d4_int = cond_sharding_constraint(d4_int, shard_pphh)
     d4 = jnp.einsum("adil, dblj -> abij", d4_int, t2)
-    d4 = with_sharding_constraint(d4, shard_pphh)
+    d4 = cond_sharding_constraint(d4, shard_pphh)
     H2 = add_AB_IJ(H2, 0.5 * d4)
     
     d5_int = jnp.einsum("cdij, cdkl -> ijkl", t2, v_pphh, optimize="optimal")
     d5 = jnp.einsum("ijkl, abkl -> abij", d5_int, t2)
-    d5 = with_sharding_constraint(d5, shard_pphh)
+    d5 = cond_sharding_constraint(d5, shard_pphh)
     H2 = H2.at[:].add(0.25 * d5)
     
     return H2
@@ -196,28 +201,28 @@ def t2_H2_dense_part2(H2, t1, t2, v_pphh, v_phph, v_phhh, v_hhhh, shard_pphh, sh
     """Diagrams 6 to 10"""
     d6_int = jnp.einsum("ak, klij -> alij", t1, v_hhhh)
     d6 = jnp.einsum("alij, bl -> abij", d6_int, t1)
-    d6 = with_sharding_constraint(d6, shard_pphh)
+    d6 = cond_sharding_constraint(d6, shard_pphh)
     H2 = add_AB(H2, 0.5 * d6)
     
     d7_int = jnp.einsum("cj, bkci -> bkji", t1, v_phph)
     d7 = jnp.einsum("ak, bkji -> abij", t1, d7_int)
-    d7 = with_sharding_constraint(d7, shard_pphh)
+    d7 = cond_sharding_constraint(d7, shard_pphh)
     H2 = add_AB_IJ(H2, -d7)
     
     d8_int = jnp.einsum("cikl, ck -> il", v_phhh, t1)
     d8 = jnp.einsum("il, ablj -> abij", d8_int, t2)
-    d8 = with_sharding_constraint(d8, shard_pphh)
+    d8 = cond_sharding_constraint(d8, shard_pphh)
     H2 = add_IJ(H2, -d8)
     
     d9_int = jnp.einsum("cikl, al -> ciak", v_phhh, t1)
-    d9_int = with_sharding_constraint(d9_int, shard_phph)
+    d9_int = cond_sharding_constraint(d9_int, shard_phph)
     d9 = jnp.einsum("ciak, bcjk -> abij", d9_int, t2)
-    d9 = with_sharding_constraint(d9, shard_pphh)
+    d9 = cond_sharding_constraint(d9, shard_pphh)
     H2 = add_AB_IJ(H2, -d9)
     
     d10_int = jnp.einsum("cjkl, ci -> jkli", v_phhh, t1) 
     d10 = jnp.einsum("jkli, abkl -> abij", d10_int, t2)
-    d10 = with_sharding_constraint(d10, shard_pphh)
+    d10 = cond_sharding_constraint(d10, shard_pphh)
     H2 = add_IJ(H2, 0.5 * d10)
     
     return H2
@@ -228,32 +233,32 @@ def t2_H2_dense_part3(H2, t1, t2, v_pphh, v_phph, v_phhh, v_hhhh, shard_pphh, sh
     d11_int1 = jnp.einsum("cjkl, ci -> jkli", v_phhh, t1) 
     d11_int2 = jnp.einsum("jkli, ak -> alij", d11_int1, t1) 
     d11 = jnp.einsum("alij, bl -> abij", d11_int2, t1)
-    d11 = with_sharding_constraint(d11, shard_pphh)
+    d11 = cond_sharding_constraint(d11, shard_pphh)
     H2 = add_AB_IJ(H2, 0.5 * d11)
 
     d12_int1 = jnp.einsum("cdkl, ci -> dkli", v_pphh, t1) 
     d12_int2 = jnp.einsum("dkli, dj -> klij", d12_int1, t1) 
     d12 = jnp.einsum("klij, abkl -> abij", d12_int2, t2)
-    d12 = with_sharding_constraint(d12, shard_pphh)
+    d12 = cond_sharding_constraint(d12, shard_pphh)
     H2 = add_IJ(H2, 0.25 * d12)
 
     d13_int1 = jnp.einsum("cdij, cdkl -> ijkl", t2, v_pphh, optimize="optimal") 
     d13_int2 = jnp.einsum("ijkl, ak -> ijal", d13_int1, t1) 
     d13 = jnp.einsum("ijal, bl -> abij", d13_int2, t1)
-    d13 = with_sharding_constraint(d13, shard_pphh)
+    d13 = cond_sharding_constraint(d13, shard_pphh)
     H2 = add_AB(H2, 0.25 * d13)
 
     d14_A = jnp.einsum("cdkl, ci -> dkli", v_pphh, t1)
     d14_B = jnp.einsum("adkj, dkli -> alij", t2, d14_A)
     d14 = jnp.einsum("alij, bl -> abij", d14_B, t1)
-    d14 = with_sharding_constraint(d14, shard_pphh)
+    d14 = cond_sharding_constraint(d14, shard_pphh)
     H2 = add_AB_IJ(H2, d14)
 
     d15_int1 = jnp.einsum("cdkl, ci -> dkli", v_pphh, t1) 
     d15_int2 = jnp.einsum("dkli, dj -> klij", d15_int1, t1) 
     d15_int3 = jnp.einsum("klij, ak -> alij", d15_int2, t1) 
     d15 = jnp.einsum("alij, bl -> abij", d15_int3, t1)
-    d15 = with_sharding_constraint(d15, shard_pphh)
+    d15 = cond_sharding_constraint(d15, shard_pphh)
     H2 = add_AB_IJ(H2, 0.25 * d15)
 
     return H2
