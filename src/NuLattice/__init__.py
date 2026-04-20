a87f46952f380fc6c38b6c871088e22127671c12
vivek-booshan <vivek.booshan@gmail.com>
2026-04-20 18:29:42 -0400

    for now, while multiple backends exist, remove init imports

--- src/NuLattice/__init__.py
+++ src/NuLattice/__init__.py
@@ -1,23 +1,12 @@
 import sys
 
-from .utils._types import (
-    OneBodyOperator,
-    TwoBodyOperator,
-    ThreeBodyOperator,
-)
 
 from .utils import (
     references,
     constants,
-    _types,
-    _torch_types,
 )
 
 sys.modules["NuLattice.constants"] = constants
 sys.modules["NuLattice.references"] = references
-sys.modules["NuLattice._torch_types"] = _torch_types
-sys.modules["NuLattice._types"] = _types
-
-__all__ = ["OneBodyOperator", "TwoBodyOperator", "ThreeBodyOperator"]
 
 
