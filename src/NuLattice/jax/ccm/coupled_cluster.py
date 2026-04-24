60dff21d3161dd35a445ddd6cf220eb43e96f704
vivek-booshan <vivek.booshan@gmail.com>
2026-04-24 16:29:34 -0400

    switch from np to jnp

--- src/NuLattice/jax/ccm/coupled_cluster.py
+++ src/NuLattice/jax/ccm/coupled_cluster.py
@@ -4,7 +4,6 @@ from collections import deque
 
 import jax
 import jax.numpy as jnp
-import numpy as np
 from jax.sharding import PartitionSpec as P, NamedSharding
 
 from NuLattice.utils._jax_types import Chef
@@ -309,7 +308,7 @@ def ccsd_solver(
                 rhs = rhs.at[size].set(-1.0)
 
                 try:
-                    c = np.linalg.solve(A, rhs)[:size]
+                    c = jnp.linalg.solve(A, rhs)[:size]
                     t1_new_diis = jnp.zeros_like(t1)
                     t2_new_diis = jnp.zeros_like(t2)
 
