56da3fe54eab825d78eda1257ddeff91d1d8b5d4
vivek-booshan <vivek.booshan@gmail.com>
2026-05-29 16:47:34 -0400

    fix spin, isospin order in generate alpha and comments, add proton and neutron function, add doubly magics

--- src/NuLattice/utils/constants.py
+++ src/NuLattice/utils/constants.py
@@ -10,12 +10,11 @@ MASS  = 0.5*(MASS_P+MASS_N)
 
 
 def generate_alpha(coords):
-    """Generates 4 states (all spin/isospin combos) for each (x, y, z) coordinate."""
     reference = []
     for x, y, z in coords:
-        for tz in [0, 1]:
-            for sz in [0, 1]:
-                reference.append([x, y, z, sz, tz])
+        for sz in [0, 1]:
+            for tz in [0, 1]:
+                reference.append([x, y, z, tz, sz])
     return reference
 
 def generate_deuteron(coords):
@@ -23,6 +22,20 @@ def generate_deuteron(coords):
     for coord in coords:
         reference.append([*coord, 0, 0])
         reference.append([*coord, 1, 0])
+    return reference
+
+def generate_proton(coords):
+    reference = []
+    for coord in coords:
+        reference.append([*coord, 0, 0])
+        reference.append([*coord, 0, 1])
+    return reference
+
+def generate_neutron(coords):
+    reference = []
+    for coord in coords:
+        reference.append([*coord, 1, 0])
+        reference.append([*coord, 1, 1])
     return reference
 
 def generate_triton(coords):
@@ -42,7 +55,7 @@ def generate_helion(coords):
     return reference
 
 class ReferenceState:
-    # [x, y, z, spin, isospin]
+    # [x, y, z, isospin, spin]
 
     H2_GS = generate_deuteron([(0, 0, 0)])
     H3_GS = generate_triton([(0, 0, 0)])
@@ -61,6 +74,10 @@ class ReferenceState:
 
     O16_GS = generate_alpha([(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)])
     O16_EX = generate_alpha([(0, 0, 0), (1, 0, 0), (0, 1, 0), (1, 1, 0)])
+    O28_GS = (
+        generate_alpha([(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)]) + 
+        generate_neutron([(1, 1, 0), (1, 0, 1), (0, 1, 1), (1, 1, 1), (2, 0, 0), (0, 2, 0)])
+    )
 
     NE20_GS = generate_alpha([(0, 0, 0), (0, 0, 1), (0, 0, 2), (1, 0, 1), (0, 1, 1)])
     MG24_GS = generate_alpha([(1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 0, 1), (0, 1, 1), (2, 1, 1)])
@@ -68,6 +85,70 @@ class ReferenceState:
     S32_GS  = generate_alpha([(1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 0, 1), (0, 1, 1), (2, 1, 1), (1, 2, 1), (0, 1, 0)])
     AR36_GS = generate_alpha([(1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 0, 1), (0, 1, 1), (2, 1, 1), (1, 2, 1), (0, 1, 0), (2, 1, 0)])
     CA40_GS = generate_alpha([(1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 0, 1), (0, 1, 1), (2, 1, 1), (1, 2, 1), (2, 0, 1), (2, 1, 2), (2, 0, 2)])
+    CA48_GS = (
+        generate_alpha([
+           (1, 1, 0),
+           (1, 1, 1),
+           (1, 1, 2),
+           (1, 0, 1),
+           (0, 1, 1),
+           (2, 1, 1),
+           (1, 2, 1),
+           (2, 0, 1),
+           (2, 1, 2),
+           (2, 0, 2),
+        ]) +
+        generate_neutron([
+             (0, 1, 0),
+             (2, 1, 0),
+             (1, 0, 0),
+             (1, 2, 0),
+         ])
+    
+    )
+
+    NI48_GS = (
+        generate_alpha([(1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 0, 1), (0, 1, 1), (2, 1, 1), (1, 2, 1), (2, 0, 1), (2, 1, 2), (2, 0, 2)]) +
+        generate_proton([(0, 1, 0), (2, 1, 0), (1, 0, 0), (1, 2, 0)])
+    )
+    NI78_GS = (
+        generate_alpha([
+            (1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 0, 1), (0, 1, 1), (2, 1, 1), (1, 2, 1),
+            (2, 0, 1), (2, 1, 2), (2, 0, 2), (0, 2, 1), (1, 2, 2), (0, 1, 2), (2, 2, 1)
+        ]) +
+        generate_neutron([
+            (0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1), (2, 0, 0), (0, 2, 0), 
+            (0, 0, 2), (2, 2, 0), (2, 0, 2), (0, 2, 2), (1, 1, 3)
+        ])
+    )
+    SN100_GS = generate_alpha([ # fill 3x3x3 grid except 000 and 222
+        (x, y, z) for x in range(3) for y in range(3) for z in range(3) 
+        if (x, y, z) not in [(0, 0, 0), (2, 2, 2)]
+    ])
+    SN132_GS = (
+        generate_alpha([
+            (x, y, z) for x in range(3) for y in range(3) for z in range(3) 
+            if (x, y, z) not in [(0, 0, 0), (2, 2, 2)]
+        ]) +
+        generate_neutron([(0, 0, 0), (2, 2, 2)]) + # Fill the missing core corner sites
+        generate_neutron([ # Boundary caps
+            (3, 1, 1), (1, 3, 1), (1, 1, 3), (-1, 1, 1), (1, -1, 1), (1, 1, -1),
+            (3, 2, 1), (1, 3, 2), (2, 1, 3), (-1, 0, 1), (1, -1, 0), (0, 1, -1),
+            (2, 2, 3), (3, 2, 2)
+        ])
+    )
+
+    # Pb-208: 41 Alphas + 22 neutron pairs forming a close-packed sphere inside a 4x4x4 cube
+    PB208_GS = (
+        generate_alpha([
+            (x, y, z) for x in range(4) for y in range(4) for z in range(4)
+            if (x-1.5)**2 + (y-1.5)**2 + (z-1.5)**2 <= 2.25
+        ]) +
+        generate_neutron([
+            (x, y, z) for x in range(4) for y in range(4) for z in range(4)
+            if 2.25 < (x-1.5)**2 + (y-1.5)**2 + (z-1.5)**2 <= 3.25
+        ])
+    )
 
     @staticmethod
     def holes(ref, basis):
