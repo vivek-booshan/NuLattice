3a29649a289f8ddd9c21845d3f4bdac50ebc8e49
vivek-booshan <vivek.booshan@gmail.com>
2026-06-25 13:55:52 -0400

    add argparse to FCI example

--- examples/Example_FCI.py
+++ examples/Example_FCI.py
@@ -1,121 +1,129 @@
-import sys, pathlib
-sys.path.append(str(pathlib.Path(__file__).parent / ".."))
-import NuLattice.lattice as lat
-import NuLattice.FCI.few_body_diagonalization as fbd
+import argparse
 from scipy.sparse.linalg import eigsh as arpack_eigsh
 
-# Initialize lattice
-# Note: on a laptop, an estimate is that H2 can be solved on L=10, He3 and H3 on L=6, and He4 on L=4 
-thisL = 3
-a_lat = 2.0 # lattice spacing in fm
+import NuLattice.cpu.lattice as lat
+import NuLattice.cpu.fci.few_body_diagonalization as fbd
 
-phys_unit = lat.phys_unit(a_lat)
+def parse():
+    parser = argparse.ArgumentParser(description="Run a NuLattice FCI calculation.")
+    parser.add_argument("--L", type=int, default=3, help="Lattice size L. Default is 3")
+    parser.add_argument("--a_lat", type=float, default=2.0, help="Lattice spacing in fm")
 
-my_basis = lat.get_sp_basis(thisL)
-nstat =  len(my_basis)
-print("number of single-particle states =", nstat)
-lattice = lat.get_lattice(thisL)
-nsite = len(lattice)
-print("number of lattice sites =", nsite)
+    parser.add_argument("--vT1", type=float, default=-9.0, help="S-wave isospin-triplet contact")
+    parser.add_argument("--vS1", type=float, default=-9.0, help="S-wave spin-triplet contact")
+    parser.add_argument("--cE", type=float, default=6.0, help="Three-body contact")
 
-# Compute operators for kinetic energy, two-body contacts, and three-body contact
+def main():
+    args = parse()
 
-vT1=-8.0  #S-wave triplet contact
-vS1=-8.0  #S-wave triplet contact
-cE = 5.5  #three-body contact
+    L = args.L
+    a_lat = args.a_lat
+    vT1 = args.vT1
+    vS1 = args.vS1
+    cE = args.cE
 
-myTkin=lat.Tkin(lattice, thisL)
-print("number of matrix elements from kinetic energy", len(myTkin))
+    phys_unit = lat.phys_unit(a_lat)
 
-mycontact=lat.contacts(vT1, vS1, lattice, thisL)
-print("number of matrix elements from two-body contacts", len(mycontact))
+    my_basis = lat.get_sp_basis(L)
+    nstat =  len(my_basis)
+    print("number of single-particle states =", nstat)
+    lattice = lat.get_lattice(L)
+    nsite = len(lattice)
+    print("number of lattice sites =", nsite)
 
-my3body=lat.NNNcontact(cE, lattice, thisL)
-print("number of matrix elements from three-body contacts", len(my3body))
+    # Compute operators for kinetic energy, two-body contacts, and three-body contact
+    myTkin=lat.Tkin(lattice, L)
+    print("number of matrix elements from kinetic energy", len(myTkin))
 
-# Compute the deuteron
-print("Computing deuteron")
-# additive quantum numbers
-numpart=2 # number of nucleons
-tz = 0    # twice the value of isospin projection
-sz = 2    # twice the value of spin projection
+    mycontact=lat.contacts(vT1, vS1, lattice, L)
+    print("number of matrix elements from two-body contacts", len(mycontact))
 
-# get two-body basis as a dictionary for lookup
-H2_lookup = fbd.get_many_body_states(my_basis, numpart, total_tz=tz, total_sz=sz)
-print("matrix dimension:", len(H2_lookup))
+    my3body=lat.NNNcontact(cE, lattice, L)
+    print("number of matrix elements from three-body contacts", len(my3body))
 
-# make scipy.sparse.csr_matrix for kinetic energy 
-T2_csr_mat = fbd.get_csr_matrix_scalar_op(H2_lookup, myTkin, nstat)
-print("kinetic energy done")
+    # Compute the deuteron
+    print("Computing deuteron")
+    # additive quantum numbers
+    numpart=2 # number of nucleons
+    tz = 0    # twice the value of isospin projection
+    sz = 2    # twice the value of spin projection
 
-# make scipy.sparse.csr_matrix for 2-body interactions 
-V2_csr_mat = fbd.get_csr_matrix_scalar_op(H2_lookup, mycontact, nstat)
-print("2-body interaction done")
+    # get two-body basis as a dictionary for lookup
+    H2_lookup = fbd.get_many_body_states(my_basis, numpart, total_tz=tz, total_sz=sz)
+    print("matrix dimension:", len(H2_lookup))
 
-# add all into Hamiltonian
-H2_csr_mat = T2_csr_mat + V2_csr_mat
+    # make scipy.sparse.csr_matrix for kinetic energy 
+    T2_csr_mat = fbd.get_csr_matrix_scalar_op(H2_lookup, myTkin, nstat)
+    print("kinetic energy done")
 
-# compute lowest eigenvalue(s)
-k_eig=10  # number of eigenvalues
-vals, vecs = arpack_eigsh(H2_csr_mat, k=k_eig, which='SA')
-print("Energies (MeV):", vals*phys_unit)
+    # make scipy.sparse.csr_matrix for 2-body interactions 
+    V2_csr_mat = fbd.get_csr_matrix_scalar_op(H2_lookup, mycontact, nstat)
+    print("2-body interaction done")
 
-# Compute He3
-print("Computing 3He")
-numpart=3
-tz = -1 # twice the value
-sz = -1 # twice the value
+    # add all into Hamiltonian
+    H2_csr_mat = T2_csr_mat + V2_csr_mat
 
-# get three-body basis as a dictionary for lookup
-He3_lookup = fbd.get_many_body_states(my_basis, numpart, total_tz=tz, total_sz=sz)
-print("matrix dimension:", len(He3_lookup))
+    # compute lowest eigenvalue(s)
+    k_eig=10  # number of eigenvalues
+    vals, vecs = arpack_eigsh(H2_csr_mat, k=k_eig, which='SA')
+    print("Energies (MeV):", vals*phys_unit)
 
-# make scipy.sparse.csr_matrix for kinetic energy 
-T3_csr_mat = fbd.get_csr_matrix_scalar_op(He3_lookup, myTkin, nstat)
-print("kinetic energy done")
+    # Compute He3
+    print("Computing 3He")
+    numpart=3
+    tz = -1 # twice the value
+    sz = -1 # twice the value
 
-# make scipy.sparse.csr_matrix for 2-body interaction 
-V3_csr_mat = fbd.get_csr_matrix_scalar_op(He3_lookup, mycontact, nstat)
-print("2-body interaction done")
+    # get three-body basis as a dictionary for lookup
+    He3_lookup = fbd.get_many_body_states(my_basis, numpart, total_tz=tz, total_sz=sz)
+    print("matrix dimension:", len(He3_lookup))
 
-# make scipy.sparse.csr_matrix for 3-body interaction 
-W3_csr_mat = fbd.get_csr_matrix_scalar_op(He3_lookup, my3body, nstat)
-print("3-body interaction done")
+    # make scipy.sparse.csr_matrix for kinetic energy 
+    T3_csr_mat = fbd.get_csr_matrix_scalar_op(He3_lookup, myTkin, nstat)
+    print("kinetic energy done")
 
-# add all into Hamiltonian
-H3_csr_mat = T3_csr_mat + V3_csr_mat + W3_csr_mat
+    # make scipy.sparse.csr_matrix for 2-body interaction 
+    V3_csr_mat = fbd.get_csr_matrix_scalar_op(He3_lookup, mycontact, nstat)
+    print("2-body interaction done")
 
-# compute lowest eigenvalue(s)
-k_eig=10  # number of eigenvalues
-vals, vecs = arpack_eigsh(H3_csr_mat, k=k_eig, which='SA')
-print("Energies (MeV):", vals*phys_unit)
+    # make scipy.sparse.csr_matrix for 3-body interaction 
+    W3_csr_mat = fbd.get_csr_matrix_scalar_op(He3_lookup, my3body, nstat)
+    print("3-body interaction done")
 
-# Compute He4
-print("Computing 4He")
-numpart=4
-tz = 0 # twice the value
-sz = 0 # twice the value
+    # add all into Hamiltonian
+    H3_csr_mat = T3_csr_mat + V3_csr_mat + W3_csr_mat
 
-# get four-body basis as a dictionary for lookup
-He4_lookup = fbd.get_many_body_states(my_basis, numpart, total_tz=tz, total_sz=sz)
-print("matrix dimension:", len(He4_lookup))
+    # compute lowest eigenvalue(s)
+    k_eig=10  # number of eigenvalues
+    vals, vecs = arpack_eigsh(H3_csr_mat, k=k_eig, which='SA')
+    print("Energies (MeV):", vals*phys_unit)
 
-# make scipy.sparse.csr_matrix for kinetic energy 
-T4_csr_mat = fbd.get_csr_matrix_scalar_op(He4_lookup, myTkin, nstat)
-print("kinetic energy done")
+    # Compute He4
+    print("Computing 4He")
+    numpart=4
+    tz = 0 # twice the value
+    sz = 0 # twice the value
 
-# make scipy.sparse.csr_matrix for 2-body interaction 
-V4_csr_mat = fbd.get_csr_matrix_scalar_op(He4_lookup, mycontact, nstat)
-print("2-body interaction done")
+    # get four-body basis as a dictionary for lookup
+    He4_lookup = fbd.get_many_body_states(my_basis, numpart, total_tz=tz, total_sz=sz)
+    print("matrix dimension:", len(He4_lookup))
 
-# make scipy.sparse.csr_matrix for 3-body interaction 
-W4_csr_mat = fbd.get_csr_matrix_scalar_op(He4_lookup, my3body, nstat)
-print("3-body interaction done")
+    # make scipy.sparse.csr_matrix for kinetic energy 
+    T4_csr_mat = fbd.get_csr_matrix_scalar_op(He4_lookup, myTkin, nstat)
+    print("kinetic energy done")
 
-# add all into Hamiltonian
-H4_csr_mat = T4_csr_mat + V4_csr_mat + W4_csr_mat
+    # make scipy.sparse.csr_matrix for 2-body interaction 
+    V4_csr_mat = fbd.get_csr_matrix_scalar_op(He4_lookup, mycontact, nstat)
+    print("2-body interaction done")
 
-# compute lowest eigenvalue(s)
-k_eig=2  # number of eigenvalues
-vals, vecs = arpack_eigsh(H4_csr_mat, k=k_eig, which='SA')
-print("Energies (MeV):", vals*phys_unit)
+    # make scipy.sparse.csr_matrix for 3-body interaction 
+    W4_csr_mat = fbd.get_csr_matrix_scalar_op(He4_lookup, my3body, nstat)
+    print("3-body interaction done")
+
+    # add all into Hamiltonian
+    H4_csr_mat = T4_csr_mat + V4_csr_mat + W4_csr_mat
+
+    # compute lowest eigenvalue(s)
+    k_eig=2  # number of eigenvalues
+    vals, vecs = arpack_eigsh(H4_csr_mat, k=k_eig, which='SA')
+    print("Energies (MeV):", vals*phys_unit)
