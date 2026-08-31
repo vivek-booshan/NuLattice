125d599ba0db080278a8c7dfa448353ea9e63dd9
vivek-booshan <vivek.booshan@gmail.com>
2026-08-31 11:34:40 -0400

    remove list_to_sparse

--- examples/Example_interaction_2016B.py
+++ examples/Example_interaction_2016B.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     v_NL=twbo.shortRangeV_2body(lattice, myL, 0, sNL, cNL , verbose=verbose)
     iso_ops = [obo.pauli_tau_x(lattice, myL), obo.pauli_tau_y(lattice, myL), obo.pauli_tau_z(lattice, myL)]
     for op in iso_ops:
-        v_NL += twbo.shortRangeV_2body(lattice, myL, 0, sNL, cINL, verbose = verbose, op1b = obo.list_to_sparse1b(op))
+        v_NL += twbo.shortRangeV_2body(lattice, myL, 0, sNL, cINL, verbose = verbose, op1b = op)
 
     cL = -0.01013 / a
     sL = 0.81
@@ -34,11 +34,11 @@ if __name__ == '__main__':
     sp_ops = [obo.pauli_spin_x(lattice, myL), obo.pauli_spin_y(lattice, myL), obo.pauli_spin_z(lattice, myL)]
     v_L = twbo.shortRangeV_2body(lattice, myL, sL, 0, cL, verbose=verbose)
     for op in sp_ops:
-        v_L += twbo.shortRangeV_2body(lattice, myL, sL, 0, cSL, verbose = verbose, op1b = obo.list_to_sparse1b(op))
+        v_L += twbo.shortRangeV_2body(lattice, myL, sL, 0, cSL, verbose = verbose, op1b = op)
     for op in iso_ops:
-        v_L += twbo.shortRangeV_2body(lattice, myL, sL, 0, cIL, verbose = verbose, op1b = obo.list_to_sparse1b(op))
+        v_L += twbo.shortRangeV_2body(lattice, myL, sL, 0, cIL, verbose = verbose, op1b = op)
         for op2 in sp_ops:
-            op1b = obo.list_to_sparse1b(op) @ obo.list_to_sparse1b(op2) 
+            op1b = op @ op2 
             v_L += twbo.shortRangeV_2body(lattice, myL, sL, 0, cSL, verbose = verbose, op1b = op1b)
     
     my_VNN = twbo.sparse_to_list_2body(v_NL+v_L+v_OPE, myL)
